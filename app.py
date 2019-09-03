9d3f8f08326f08637747e3e5aad582edfb46189b
Marta Matros <matros.marta@gmail.com>
2019-09-03 14:07:42 +0200

    initial setup

---
+++ app.py
@@ -0,0 +1,18 @@
+import boto3
+import config
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+@app.route("/")
+
+def main():
+  return render_template('index.html')
+
+@app.route('/images')
+def list_of_images():
+  return render_template('images.html')
+  
+if __name__ == "__main__":
+  app.run()
