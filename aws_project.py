063d69afe0447116a3006281e0952f49b89bc2c9
Marta Matros <matros.marta@gmail.com>
2019-09-04 16:54:13 +0200

    test

--- aws_project.py
+++ aws_project.py
@@ -4,21 +4,24 @@ import logging
 import os
 from flask import Flask, render_template
 
+import config
+
 app = Flask(__name__)
 @app.route("/")
 
 def main():
   s3 = boto3.client(
     's3',
-    aws_access_key_id = os.environ.get('ACCESS_KEY'),
-    aws_secret_access_key = os.environ.get('SECRET_KEY'),
+    aws_access_key_id = config.ACCESS_KEY,
+    aws_secret_access_key = config.SECRET_KEY,
   )
   response = s3.generate_presigned_post(Bucket='aws-projekt', Key='uploads/${filename}', Fields={}, Conditions=[],ExpiresIn=2592000)
-  return render_template('index.html', config=response)
+  print(response)
+  return render_template('index.html')
 
 @app.route('/images')
 def list_of_images():
   return render_template('images.html')
-  
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80)
